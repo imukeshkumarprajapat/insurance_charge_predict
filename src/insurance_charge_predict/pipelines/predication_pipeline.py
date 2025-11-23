e19bc88f2be3581d8ce30504d572dd079e553e3a
imukeshkumarprajapa <imukeshkumarprajapat@gmail.com>
2025-11-23 14:32:27 +0530

    resolve issue

--- src/insurance_charge_predict/pipelines/predication_pipeline.py
+++ src/insurance_charge_predict/pipelines/predication_pipeline.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src.insurance_charge_predict.exception import CustomException
 from src.insurance_charge_predict.utils import load_object
 
-
+import os
 class PredictionPipline:
     def __init__(self):
         pass
@@ -12,8 +12,13 @@ class PredictionPipline:
 
     def predict(self, features):
         try:
-            model_path="artifacts\model.pkl"
-            preprocessor_path="artifacts\preprocessor.pkl"
+            # model_path="artifacts\model.pkl"
+            # preprocessor_path="artifacts\preprocessor.pkl"
+        
+
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
